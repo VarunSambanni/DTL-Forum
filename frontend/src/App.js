01aa0ac36bc15f0a70cceef15a2f27db67466a3d
varunsambanni <varunsambanni@gmail.com>
2022-09-19 22:38:45 +0530

    removed unwanted imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
