11168894cb983c5ac04fd2d498d29360aec4096b
varunsambanni <varunsambanni@gmail.com>
2023-03-01 19:05:17 +0530

    ui changes

--- frontend/src/pages/PostAnnouncement.js
+++ frontend/src/pages/PostAnnouncement.js
@@ -10,15 +10,16 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import Logout from '../utils/Logout';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import ReactMarkdown from "react-markdown";
 
 
 const PostAnnouncement = () => {
     document.title = 'Add Announcement-Admin - Interax';
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [category, setCategory] = useState('1st Cat');
-    const [checked, setChecked] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+    const [previewOption, setPreviewOption] = useState('html');
+    const [showPreview, setShowPreview] = useState("previewContainer2");
 
     const postAnnouncementHandler = () => {
         if (title.length < 5) {
@@ -64,6 +65,15 @@ const PostAnnouncement = () => {
             })
     }
 
+    const previewToggleHanlder = () => {
+        if (showPreview === 'previewContainer') {
+            setShowPreview('previewContainer2')
+        }
+        else {
+            setShowPreview('previewContainer')
+        }
+    }
+
     return <>
         <ToastContainer autoClose={4000} hideProgressBar={true} limit={1} closeButton={true} position={'top-right'}></ToastContainer>
         <div className='linearProgressContainer'>
@@ -87,6 +97,24 @@ const PostAnnouncement = () => {
                 <p style={{ 'padding': '0 0.6em' }}><i><b> Note:</b> For html/markdown posts, add <b>.html/.md</b> at the end of the title</i></p>
                 <TextField variant='outlined' size='small' sx={{ margin: '0.5em' }} label='Title' value={title} onChange={(e) => { setTitle(e.target.value) }}></TextField>
                 <TextField multiline minRows={12} maxRows={12} variant='outlined' size='small' sx={{ margin: '0.5em' }} inputProps={{ style: { fontSize: '0.96rem' } }} label='Body' value={body} onChange={(e) => { setBody(e.target.value) }}></TextField>
+                <div className={showPreview} >
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <button className='button' onClick={previewToggleHanlder}> PREVIEW</button>
+                        <div>
+                            Option :
+                            <Select size='small' sx={{ margin: '0.5em', maxWidth: "8em", alignSelf: 'left' }} value={previewOption} onChange={(e) => setPreviewOption(e.target.value)} >
+                                <MenuItem value={'html'}>HTML</MenuItem>
+                                <MenuItem value={'md'}>Markdown</MenuItem>
+                            </Select>
+                        </div>
+                    </div>
+                    <hr></hr>
+
+                    {
+                        previewOption === 'html' ? <pre style={{ maxHeight: '40em' }} dangerouslySetInnerHTML={{ __html: body }}></pre>
+                            : <ReactMarkdown>{body}</ReactMarkdown>
+                    }
+                </div>
                 <div className='buttonWrapper'>
                     <button className='button' style={{ margin: '4em' }} onClick={postAnnouncementHandler}><p className='centerText buttonText' >POST</p></button>
                 </div>
