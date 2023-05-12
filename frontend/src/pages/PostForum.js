406374eddd68938d68b16b66cd15d30a792c2ff0
varunsambanni <varunsambanni@gmail.com>
2023-05-12 12:28:39 +0530

    added reward system

--- frontend/src/pages/PostForum.js
+++ frontend/src/pages/PostForum.js
@@ -132,7 +132,9 @@ const PostForum = () => {
                 <div className='forumWrapper'>
                     <div className='pageHeading'>Post</div>
                     <div className='postsWrapper' style={{ backgroundColor: 'rgb(242, 242, 255)' }} >
-                        <p style={{ 'padding': '0 0.6em' }}><i><b> Note:</b> For html/markdown posts, add <b>.html/.md</b> at the end of the title</i></p>
+                        <div className='note'>
+                            <b> Note:</b> For html/markdown posts, add <b>.html/.md</b> at the end of the title.
+                        </div>
                         <TextField variant='outlined' size='small' sx={{ margin: '0.5em' }} label='Title' value={title} onChange={(e) => { setTitle(e.target.value) }}></TextField>
                         <TextField multiline minRows={8} maxRows={8} variant='outlined' size='small' sx={{ margin: '0.5em' }} inputProps={{ style: { fontSize: '0.96rem' } }} label='Body' value={body} onChange={(e) => { setBody(e.target.value) }}></TextField>
                         <div className={showPreview} >
--- frontend/src/pages/UserInfo.js
+++ frontend/src/pages/UserInfo.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { TextField, Button, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import '../index.css';
@@ -14,9 +14,43 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
 import AnnouncementIcon from '@mui/icons-material/Announcement';
 import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
+import { LinearProgress } from '@mui/material';
+import GradeIcon from '@mui/icons-material/Grade';
 
 const UserInfo = ({ isAdmin }) => {
     const [showDrawer, setShowDrawer] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [userScore, setUserScore] = useState(0);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://dtlforum-backend.vercel.app/getScore', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('token')
+            },
+            body: JSON.stringify({ username: localStorage.getItem('username') })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log("data    ", data);
+                setIsLoading(false);
+                if (data.success === false) {
+                    toast.error(data.msg, { autoClose: 4000 });
+                }
+                else {
+                    setUserScore(data.score);
+                }
+            })
+            .catch(err => {
+                setIsLoading(false);
+                console.log("Error connecting to server");
+                toast.error("Error connecting to server", { autoClose: 4000 });
+            })
+    }, []);
+
 
     if (isAdmin === true) {
         console.log("Here right");
@@ -29,6 +63,7 @@ const UserInfo = ({ isAdmin }) => {
         <>
             <ToastContainer autoClose={4000} hideProgressBar={true} limit={1} closeButton={true} position={'top-right'}></ToastContainer>
             <div className='linearProgressContainer'>
+                {isLoading && <LinearProgress></LinearProgress>}
             </div>
             <div className='userBarContainer'>
                 {!isAdmin && <NavbarForum />}
@@ -69,6 +104,9 @@ const UserInfo = ({ isAdmin }) => {
                     <div className='forumWrapper'>
                         <div className='pageHeading'>User Info</div>
                         <div className='postsWrapper'>
+                            <div className='note'>
+                                <b>Note :</b> The score of a user shows how interactive and involved a user is in this community. It is calculated with the help of the user's replies, upvotes and posts.
+                            </div>
                             <div className='loginContainer' style={{ padding: '0.4em', fontSize: '0.95rem', margin: 'auto' }} >
                                 <div className='loginForm'>
                                     <h4 className='centerText'>User Info</h4>
@@ -83,6 +121,13 @@ const UserInfo = ({ isAdmin }) => {
                                     <div className='userInfoText' style={{ display: 'flex', flexDirection: 'row' }}>
                                         <h4 style={{ margin: '0.2em', padding: '0em' }}>Year: </h4>
                                         <h4 style={{ margin: '0.2em', padding: '0em', fontWeight: '100' }}>{localStorage.getItem('year')}</h4>
+                                    </div>
+                                    <div className='userInfoText' style={{ display: 'flex', flexDirection: 'row' }}>
+                                        <h4 style={{ margin: '0.2em', padding: '0em' }}>User Score: </h4>
+                                        <div className='scoreWrapper'>
+                                            <h4 style={{ margin: '0.2em', padding: '0em', fontWeight: '100' }}>{userScore > 0 ? userScore.toFixed(4) : 0}</h4>
+                                            <GradeIcon />
+                                        </div >
                                     </div>
                                 </div>
                             </div >
